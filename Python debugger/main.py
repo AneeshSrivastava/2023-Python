dbc3245dc2b4d32348b6bd03a67e63f822878712
Aneesh  Srivastava <aneesh.s@safe.security>
2023-03-09 23:29:09 +0530

    python debugging code

---
+++ Python debugger/main.py
@@ -0,0 +1,13 @@
+#Source: https://www.youtube.com/watch?v=7qZBwhSlfOo
+
+def sample_func(num):
+    count = 10
+    for i in range(num):
+        count = count + num
+    
+
+def main():
+    sample_func(12)
+
+if __name__ == '__main__':
+    main()
